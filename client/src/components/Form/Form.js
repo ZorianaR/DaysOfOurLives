ad9689d8b8f3cb91de235b9fad2581c576710d01
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2022-10-26 15:31:56 +0300

    Created a form for adding life moments

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import useStyles from './styles';
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '', title: '', message: '', tags:'', selectedFile:''
+    })
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        dispatch(createPost(postData))
+    }
+    const clear = () => {
+        
+      };
+    
     return(
-        <h1>Form</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h5">Додай момент...</Typography>
+
+                <TextField 
+                    name="creator" 
+                    variant="outlined" 
+                    label="Автор" 
+                    fullWidth value={postData.creator} 
+                    onChange={(e) => setPostData({...postData, creator: e.target.value})}/>
+
+                <TextField 
+                    name="title" 
+                    variant="outlined" 
+                    label="Момент" 
+                    fullWidth value={postData.title} 
+                    onChange={(e) => setPostData({...postData, title: e.target.value})}/>
+
+                <TextField 
+                    name="message" 
+                    variant="outlined" 
+                    label="Повідомлення" 
+                    fullWidth value={postData.message} 
+                    onChange={(e) => setPostData({...postData, message: e.target.value})}/>
+
+                <TextField 
+                    name="tags" 
+                    variant="outlined" 
+                    label="Теги" 
+                    fullWidth value={postData.tags} 
+                    onChange={(e) => setPostData({...postData, tags: e.target.value})}/>
+
+                <div className={classes.fileInput}>
+                <FileBase 
+                    type="file" 
+                    multiple={false} 
+                    onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} />
+                </div>
+                <Button 
+                    className={classes.buttonSubmit} 
+                    variant="contained" 
+                    color="primary" 
+                    size="large" 
+                    type="submit" 
+                    fullWidth>Додати
+                </Button>
+                <Button 
+                    variant="contained" 
+                    color="primary" 
+                    size="large" 
+                    onClick={clear} 
+                    fullWidth>Очистити комірки
+                </Button>
+            </form>
+
+        </Paper>
     );
 }
 
