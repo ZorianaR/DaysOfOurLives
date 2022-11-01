fc78218cbbefbedd6a38c0b58b0766c4dfa1ec9d
SolodkaSofiia <okeyguys48@gmail.com>
2022-11-01 18:09:36 +0200

    Add Update functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
-const Post = ({post}) => {
+const Post = ({post, setCurrentId}) => {
     const classes = useStyles();
     return(
         <Card className={classes.card}>
@@ -16,14 +16,14 @@ const Post = ({post}) => {
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
         <div className={classes.overlay2}>
-          <Button style={{ color: 'white' }} size="small" onClick={() => {}}><MoreHorizIcon fontSize="default" /></Button>
+          <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button>
         </div>
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" >{post.tags.map((tag) => `#${tag} `)}</Typography>
         </div>
-        {/* <Typography className={classes.title} gutterBottom variant="h5" >{post.message}</Typography> */}
+        
         <CardContent>
-          <Typography className={classes.title} gutterBottom variant="h5" >{post.message}</Typography>
+          <Typography className={classes.title}  variant="h5" gutterBottom >{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions}>
           <Button size="small" color="primary" onClick={() =>{}}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
